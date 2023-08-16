a758d2e0d7ccdb590c8b7791eec053940b877ef6
shitanshu-277 <shitanshugoyal2002@gmail.com>
2023-08-16 14:06:55 +0530

    Completed city repository

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ const express=require('express');
 const bodyParser = require("body-parser");
 
 const {PORT}=require('./config/serverConfig');
+const CityRepository = require('./repository/city-repository')
 const setupAndStartServer = async()=>{
     const app=express();
     app.use(bodyParser.json());
--- src/repository/city-repository.js
+++ src/repository/city-repository.js
@@ -7,6 +7,7 @@ class CityRepository {
             return city;
         }
         catch(error){
+            console.log("Something went wrong in the repository layer");
             throw {error};
         }
     };
@@ -18,7 +19,35 @@ class CityRepository {
                     id: cityId
                 }
             });
+            return true;
         } catch(error){
+            console.log("Something went wrong in the repository layer");
+            throw {error};
+        }
+    }
+
+    async updateCity(cityId,data){
+        try{
+            const city = await City.update(data,{
+                Where:{
+                    id:cityId
+                }
+            });
+            return city;
+        }
+        catch(error){
+            console.log("Something went wrong in the repository layer");
+            throw {error};
+        } 
+    }
+
+    async getCity(cityId){
+        try{
+            const city = await City.findByPk(cityId);
+            return City;
+        }
+        catch(error){
+            console.log("Something went wrong in the repository layer");
             throw {error};
         }
     }
