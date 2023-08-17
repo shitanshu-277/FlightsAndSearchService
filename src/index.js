8b85cfacc81d9593f3e17915c61b83a401d10e16
shitanshu-277 <shitanshugoyal2002@gmail.com>
2023-08-17 21:41:51 +0530

    Added airport seeders

--- src/index.js
+++ src/index.js
@@ -4,6 +4,8 @@ const bodyParser = require("body-parser");
 const {PORT}=require('./config/serverConfig');
 const ApiRoutes = require('./routes/index');
 
+const db=require('./models/index');
+const {City,Airport}=require('./models/index');
 const setupAndStartServer = async()=>{
     const app=express();
     app.use(bodyParser.json());
@@ -13,6 +15,26 @@ const setupAndStartServer = async()=>{
 
     app.listen(PORT,async()=>{
         console.log(`Server started at ${PORT}`);
+        if(process.env.SYNC_DB){
+            db.sequelize.sync({force:true});
+        }
+        // const city = await City.findOne({
+        //     where: {
+        //         id:15
+        //     }
+        // });
+        // const airports = await city.getAirports();
+        // // await city.addAirport(newAirport);
+        // // const newAirport = await Airport.findOne({
+        // //     where:{
+        // //         id:11
+        // //     }
+        // // });
+        // // await city.addAirport(newAirport);
+        // // await city.addAirport({
+        // //     name: 'Jindal Vijaynagar Airport'
+        // // })  
+        //console.log(city,airports); 
     });
 }
 
