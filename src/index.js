2ff7879fa876bf5a22b82acd74fde2e7219803b7
shitanshu-277 <shitanshugoyal2002@gmail.com>
2023-08-20 20:28:34 +0530

    Added airplanes

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ const {PORT}=require('./config/serverConfig');
 const ApiRoutes = require('./routes/index');
 
 const db=require('./models/index');
-const {City,Airport}=require('./models/index');
+//const {Airplane}=require('./models/index');
 const setupAndStartServer = async()=>{
     const app=express();
     app.use(bodyParser.json());
@@ -35,6 +35,9 @@ const setupAndStartServer = async()=>{
         // //     name: 'Jindal Vijaynagar Airport'
         // // })  
         //console.log(city,airports); 
+        // await Airplane.create({
+        //     modelNumber: 'Bombardier CRJ'
+        // });
     });
 }
 
